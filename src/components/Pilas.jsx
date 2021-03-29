c22d637a7ff6e75e5f88ab9173b05df1d06f7a27
Mané Firós <sendmyasith@gmail.com>
2021-03-29 18:16:01 +0200

    first commit

---
+++ src/components/Pilas.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import  Autonomia from './Autonomia';
+import  Carga from './Carga';
+
+function PilasConvencionales() {
+    return (<div>
+        <h3>Pilas convencionales</h3>
+        <p>
+            A pesar del auge de las energias renovables, 
+            las pilas convencionales siguen utilizandose a diario.
+        </p>
+        <Autonomia km={0}/>
+        <Carga carga='Sí/No'/>
+        <br />
+    </div>);
+}
+
+export default PilasConvencionales;
